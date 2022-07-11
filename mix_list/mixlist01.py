e1ed311fd63e8da4a4a2d61df98da109963df3d3
adelgit1984 <adelgit1984@users.noreply.github.com>
2022-07-11 20:26:14 +0000

    mixed lists

---
+++ mix_list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+my_list = ["192.168.0.5", 5060, "UP"]
+# this is my list
+
+print("The first item in the list (IP): " + my_list[0])
+# first print statement
+
+print("The second item in the list (port): " + str(my_list[1]))
+# second print statement
+
+print("The last item in the list (state): " + my_list[2])
+# third print statement
+
